169fdc0b4151eb0a369e09ad8d41ca66ddf500f4
yukiosz <rafaelyukioshira@gmail.com>
2024-11-08 09:42:02 -0300

    upload do projeto

---
+++ src/components/Game.jsx
@@ -0,0 +1,12 @@
+import Board from './Board.jsx'
+
+export default function Game() {
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board className='board'/>
+        </div>
+      </div>
+    );
+  }
